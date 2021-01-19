314677739bd254b02dcef9d422d25d6af4e109f8
alanmatw <alanmatw@gmail.com>
2021-01-19 20:22:10 +0530

    feat(dashboard/project): top nav grid workarounds

---
+++ zod/src/components/Dashboard/Project/Project.js
@@ -0,0 +1,21 @@
+import './Project.css';
+
+function ProjectPg() {
+    return (
+        <div className="ProjectPg">
+
+            <div className="top-nav">
+
+                <div className="left-wrapper-tn">
+                    <p className="title">zode</p>
+                </div>
+
+                <div className="right-wrapper-tn">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectPg;
--- zod/src/components/Dashboard/Project/ProjectPage.js
+++
@@ -1,9 +0,0 @@
-function ProjectPage() {
-    return (
-        <div className="ProjectPage">
-        <h2> Project Page</h2>
-        </div>
-    );
-}
-
-export default ProjectPage;
