40ccecb767956e9f381407ceaee4b58133c71be2
jamesjose03 <jamesjose03@gmail.com>
2021-01-20 18:41:23 +0530

    feat: add nav to home page

--- zod/src/components/Home/Home.js
+++ zod/src/components/Home/Home.js
@@ -1,7 +1,19 @@
+import './Home.css'
 function HomePage() {
     return (
         <div className="HomePage">
-        <h2> Home Page</h2>
+            <div className="zod-top-nav">
+                    <div className="left-wrapper-tn">
+                        <p className="title">zode</p>
+                    </div>
+
+                    <ul className = "zod-nav-items">
+                        <li>Features</li>
+                        <li>Pricing</li>
+                        <li>Login</li>
+                        <li><button className="zod-signup-navbtn">Sign Up</button></li>
+                    </ul>
+            </div>
         </div>
     );
 }
