cbb1adef1bafbe871b06569822b1187fba529327
alanmatw <alanmatw@gmail.com>
2021-03-31 23:40:47 +0530

    feat: profile icon - workarounds

--- zod/src/components/Dashboard/Project/createProject.js
+++ zod/src/components/Dashboard/Project/createProject.js
@@ -6,31 +6,29 @@ function CreateProject() {
         <div className="createPro">   
 
             <div className="cp-top-nav">
+
                 <div className="cp-left-wrapper-tn">
                     <p className="cp-title">zode</p>
                 </div>
 
-                <div className="cp-right-wrapper-tn">                    
+                <div className="cp-right-wrapper-tn">  
+                    <div class="cp-dropdown">
+                        <button class="cp-dropbtn">
+                            <p className="cp-profile-text">JD</p> 
+                        </button>
 
-                    <Dropdown>
-                        <Dropdown.Toggle className="cp-profile-circle">
-                            <p className="cp-profile-text">JD</p>
-                        </Dropdown.Toggle>
-        
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="/login">Action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                  
+                        <div class="cp-dropdown-content">
+                            <p>My Profile</p>
+                            <p>Dashboard</p>
+                            <p>Logout</p>
+                        </div>
 
+                    </div>
                 </div>
-
             </div>        
 
             <div>
-                <h1>Create new project</h1>
+                <h1></h1>
             </div>
             
         </div>
