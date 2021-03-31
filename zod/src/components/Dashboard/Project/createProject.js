6ac6f53b6231b276211bb31102988f1992fda58d
alanmatw <alanmatw@gmail.com>
2021-03-31 22:51:35 +0530

    feat(CreateProject): profile icon - dropdown

--- zod/src/components/Dashboard/Project/createProject.js
+++ zod/src/components/Dashboard/Project/createProject.js
@@ -1,18 +1,30 @@
 import './CreateProject.css';
+import { Dropdown } from 'react-bootstrap';
 
 function CreateProject() {
     return (
         <div className="createPro">   
 
-            <div className="top-nav">
-                <div className="left-wrapper-tn">
-                    <p className="title">zode</p>
+            <div className="cp-top-nav">
+                <div className="cp-left-wrapper-tn">
+                    <p className="cp-title">zode</p>
                 </div>
 
-                <div className="right-wrapper-tn">                    
-                    <div className="profile-circle">
-                        <p className="profile-text">JD</p>
-                    </div>
+                <div className="cp-right-wrapper-tn">                    
+
+                    <Dropdown>
+                        <Dropdown.Toggle className="cp-profile-circle">
+                            <p className="cp-profile-text">JD</p>
+                        </Dropdown.Toggle>
+        
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="/login">Action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                  
+
                 </div>
 
             </div>        
