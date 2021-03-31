43b1be52e511f6496d381fa950fe427ef16a3155
alanmatw <alanmatw@gmail.com>
2021-03-31 14:54:10 +0530

    feat(createProject): init

---
+++ zod/src/components/Dashboard/Project/createProject.js
@@ -0,0 +1,11 @@
+import './createProject.css';
+
+function createProject() {
+    return (
+        <div className="createPro">
+            <h1>Create new project</h1>
+        </div>
+    );
+}
+
+export default createProject;
