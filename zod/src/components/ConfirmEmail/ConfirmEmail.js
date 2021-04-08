3b106aeaa1683907c49a5395633eb9013c70c97b
jamesjose03 <jamesjose03@gmail.com>
2021-04-08 09:54:01 +0530

    feat: add timer for resend email btn

--- zod/src/components/ConfirmEmail/ConfirmEmail.js
+++ zod/src/components/ConfirmEmail/ConfirmEmail.js
@@ -3,16 +3,26 @@ import sendMsgSVG from '../../assets/send-message.svg';
 import React from 'react';
 
 function ConfirmEmail({email}) {
-    const [seconds, setSeconds] = React.useState(10);
+    const [seconds, setSeconds] = React.useState(30);
+    let [isDisabled, setDisabled] = React.useState(false);
     var btn = document.getElementById('resend-btn');
     React.useEffect(() => {
       if (seconds > 0) {
+        setDisabled(true);
         setTimeout(() => setSeconds(seconds - 1), 1000);
       } else {
+        setDisabled(false);  
         setSeconds(0);
-        btn.removeAttribute("disabled");
       }
     });
+    async function handleBtnClicked() {
+        btn.setAttribute("disabled","");
+        setSeconds(30);
+        setTimeout(()=> {
+            console.log("Timeout");
+            document.getElementById('resend-btn').removeAttribute('disabled');
+        }, 30000);
+    }
     return(
         <div className="ConfirmEmailPage">
             <span className="zod-ctitle">zode</span>
@@ -24,7 +34,7 @@ function ConfirmEmail({email}) {
                 <p>We have sent a verificaton email to {email}.<br></br>Please click the link to verify your account.</p>
             </div>
             <div className="zod-mail-timer">
-                <p>Didn't receive the mail? <button id="resend-btn" disabled>Resend</button> in {seconds} seconds.</p>
+                <p>Didn't receive the mail? <button id="resend-btn" disabled={isDisabled} onClick={handleBtnClicked.bind()}>Resend</button> in {seconds} seconds.</p>
             </div>
         </div>
     )
