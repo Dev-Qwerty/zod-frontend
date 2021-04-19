9271ceb73bbb508e0c44d8b5a1541df0b4abf54e
alanmatw <alanmatw@gmail.com>
2021-04-19 22:14:24 +0530

    feat(MyProfile): change password page to modify profile page

--- zod/src/components/Dashboard/BaseDashboard/CmpProfile.js
+++ zod/src/components/Dashboard/BaseDashboard/CmpProfile.js
@@ -14,15 +14,29 @@ function CmpProfile() {
                 
                 <div className="mp-profile-img"></div>
                 
-                <div className="mp-inp-wrapper">        
+                <div className="mp-inp-wrapper">    
+
                     <div className="mp-x">
-                        <p className="mp-label">Current Password</p>
-                        <div><input type="text" placeholder="" className="mp-inp"></input></div>
-                        <p className="mp-label">New Password</p>
-                        <div><input type="text" placeholder="" className="mp-inp"></input></div>
-                        <p className="mp-label">Confirm New Password</p>
-                        <div><input type="text" placeholder="" className="mp-inp"></input></div>                    
-                        <div><input value="Update Password" type="submit" className="mp-submit"></input></div>   
+
+                        <div className="mp-class1">
+                            
+                            <div className="mp-fname">
+                                <p className="mp-name-label">First Name</p>
+                                <div><input type="text" placeholder=""  className="mp-name-inp1"></input></div>
+                            </div>
+                            
+                            <div className="mp-lname">
+                                <p className="mp-name-label">Last Name</p>
+                                <div className="mp-name-inp"><input type="text" placeholder=""  className="mp-name-inp2"></input></div>
+                            </div>
+                        </div>
+
+                        <div className="mp-class2">
+                            <p className="mp-email">Email</p>
+                            <div><input type="text" placeholder="" className="mp-email-inp"></input></div>                            
+                            <div><input value="Update Profile" type="submit" className="mp-update-btn"></input></div> 
+                            <div><input value="Delete Account" type="submit" className="mp-delete-btn"></input></div> 
+                        </div>
                     </div>            
                 </div>
 
