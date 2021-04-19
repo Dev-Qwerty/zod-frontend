5cd5017f4dd7d649ba74a950e472e6e353d9f4cf
alanmatw <alanmatw@gmail.com>
2021-04-19 22:21:10 +0530

    feat(MyProfile): add toast

--- zod/src/components/Dashboard/BaseDashboard/CmpProfile.js
+++ zod/src/components/Dashboard/BaseDashboard/CmpProfile.js
@@ -1,12 +1,40 @@
 import './CmpProfile.css';
 import { Link } from "react-router-dom";
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 /* 
     ClassName Convention Used:-
         Eg: mp-top-nav -> MyProfile-top-nav ..
 */
 
 function CmpProfile() {
+
+    const upadteProfile = () => {
+
+        toast.info('Project Updated!', {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });     
+    }
+
+    const deleteAccount = () => {
+        
+        toast.error('Profile Deleted!', {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });     
+    }    
+
     return (
         <div className="MyProfile">
 
@@ -34,9 +62,11 @@ function CmpProfile() {
                         <div className="mp-class2">
                             <p className="mp-email">Email</p>
                             <div><input type="text" placeholder="" className="mp-email-inp"></input></div>                            
-                            <div><input value="Update Profile" type="submit" className="mp-update-btn"></input></div> 
-                            <div><input value="Delete Account" type="submit" className="mp-delete-btn"></input></div> 
+                            <div><input value="Update Profile" type="submit" className="mp-update-btn" onClick = { upadteProfile } ></input></div> 
+                            <div><input value="Delete Account" type="submit" className="mp-delete-btn"onClick = { deleteAccount } ></input></div> 
                         </div>
+
+                        <ToastContainer />
                     </div>            
                 </div>
 
