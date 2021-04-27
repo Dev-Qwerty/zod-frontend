dd7ce400fef3ed1f0efaa7558c75b14bd61a63f6
jamesjose03 <jamesjose03@gmail.com>
2021-04-27 22:54:28 +0530

    add loading btn animation and toast notifications to login page

--- zod/src/components/Login/Login.js
+++ zod/src/components/Login/Login.js
@@ -2,11 +2,17 @@ import './Login.css';
 import {Link} from 'react-router-dom';
 import firebase from 'firebase';
 import React from 'react';
+import Button from 'react-bootstrap-button-loader';
+import {toast} from 'react-toastify'; 
+
+toast.configure()
 
 const { useState } = React;
 function LoginPage() {
     const [email, setemailValue] = useState('');
     const [password, setPasswordValue] = useState('');
+    const [loading, setLoader] = useState(false);
+    const [btnText, setBtnText] = useState('Login');
     
     const handleEmailChange = (e) => setemailValue(e.target.value);
     const handlepasswordChange = (e) => setPasswordValue(e.target.value);
@@ -20,7 +26,7 @@ function LoginPage() {
                         <div className="zod-login-inputs">
                             <input type="text" placeholder="Enter email" className="zod-login-grp form-control" value={email} onChange={handleEmailChange}></input>
                             <input type="password" placeholder="Enter password" className="zod-login-grp form-control" value={password} onChange={handlepasswordChange}></input>
-                            <input type="submit" value="Login" className="zod-login-btn zod-login-grp" onClick={LoginRequest.bind(this, email, password)}/>
+                            <Button variant="success" loading={loading} className="zod-login-btn zod-login-grp" onClick={LoginRequest.bind(this, email, password, setLoader, setBtnText)}>{btnText}</Button>
                             <hr/>
                             <button type="submit" className="zod-google-btn"><img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="Google Logo"></img>Login with Google</button>
                         </div>
@@ -37,7 +43,9 @@ function LoginPage() {
     );
 }
 
-function LoginRequest(email, password) {
+function LoginRequest(email, password, setLoader, setBtnText) {
+    setLoader(true);
+    setBtnText('Logging in...');
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
         // Signed in
@@ -55,7 +63,19 @@ function LoginRequest(email, password) {
     .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
-        console.log(errorCode + ' ' + errorMessage);
+        setBtnText("Sign In");
+        setLoader(false);
+        if(errorMessage === "There is no user record corresponding to this identifier. The user may have been deleted.") {
+            toast.info("No account exists with given email. Please sign up!", {position: toast.POSITION.BOTTOM_LEFT});
+            window.location.href = window.location.protocol + '//' + window.location.host + '/signup'; 
+        }
+        else if(errorMessage === "The password is invalid or the user does not have a password.") {
+            toast.error("Invalid email or password! Please check.", {position: toast.POSITION.BOTTOM_LEFT});
+        }
+        else {
+            toast.error(errorMessage, {position: toast.POSITION.BOTTOM_LEFT})
+        }
+
     });
 }
 
