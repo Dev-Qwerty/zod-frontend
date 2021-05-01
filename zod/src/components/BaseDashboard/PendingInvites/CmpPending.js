1a1b622b00905cf9d5ffb4ee3d72e8effb44d3d6
alanmatw <alanmatw@gmail.com>
2021-05-01 14:49:49 +0530

    feat(PendingInvites): api call - axios

--- zod/src/components/BaseDashboard/PendingInvites/CmpPending.js
+++ zod/src/components/BaseDashboard/PendingInvites/CmpPending.js
@@ -1,4 +1,6 @@
 import './CmpPending.css';
+import React from 'react';
+import axios from 'axios';
 import { Link } from "react-router-dom";
 
 /* 
@@ -6,47 +8,84 @@ import { Link } from "react-router-dom";
         Eg: mp-top-nav -> MyProfile-top-nav ..
 */
 
-function CmpPending() {
-    return (
-        <div className="cmpPI">
+export default class CmpPending  extends React.Component {
 
-            <div className="cpi-wrapper">
+    constructor() {
+     
+        super();
+        this.state = {
 
-                <div className="cpi-box">
-            
-                    <div className="cpi-box-item1">
-                        <div className="cpi-box-item1-wrapper">
-                            <p className="cpiY">Project Name: Project X</p>
-                            <p className="cpiY qw">Invited By: John Doe</p>
+        }   
+
+    }   
+    
+    componentDidMount(){
+     
+        const token = localStorage.getItem('token');
+
+        const config = {
+            headers: {
+                'Authorization': token,
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin' : '*',
+            }
+        }
+    
+        axios.get('https://projectservice-zode.herokuapp.com/api/projects/invites', config)
+        .then((res) => {
+    
+            if(res.status === 200) {
+                alert(JSON.stringify(res.data))
+            } else {
+
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });         
+    }
+    
+    render() {
+        return (
+            <div className="cmpPI">
+    
+                <div className="cpi-wrapper">
+    
+                    <div className="cpi-box">
+                
+                        <div className="cpi-box-item1">
+                            <div className="cpi-box-item1-wrapper">
+                                <p className="cpiY">Project Name: Project X</p>
+                                <p className="cpiY qw">Invited By: John Doe</p>
+                            </div>
+                        </div>
+                        <div className="cpi-box-item2">
+                            <div><input value="Accept" type="submit" className="cpi-acceptBtn"></input></div>
+                        </div>
+                        <div className="cpi-box-item3">
+                            <div><input value="Reject" type="submit" className="cpi-rejectBtn"></input></div>
                         </div>
                     </div>
-                    <div className="cpi-box-item2">
-                        <div><input value="Accept" type="submit" className="cpi-acceptBtn"></input></div>
-                    </div>
-                    <div className="cpi-box-item3">
-                        <div><input value="Reject" type="submit" className="cpi-rejectBtn"></input></div>
-                    </div>
-                </div>
-
-                <div className="cpi-box">
-            
-                    <div className="cpi-box-item1">
-                        <div className="cpi-box-item1-wrapper">
-                            <p className="cpiY">Project Name: Project X</p>
-                            <p className="cpiY qw">Invited By: John Doe</p>
+    
+                    <div className="cpi-box">
+                
+                        <div className="cpi-box-item1">
+                            <div className="cpi-box-item1-wrapper">
+                                <p className="cpiY">Project Name: Project X</p>
+                                <p className="cpiY qw">Invited By: John Doe</p>
+                            </div>
+                        </div>
+                        <div className="cpi-box-item2">
+                            <div><input value="Accept" type="submit" className="cpi-acceptBtn"></input></div>
+                        </div>
+                        <div className="cpi-box-item3">
+                            <div><input value="Reject" type="submit" className="cpi-rejectBtn"></input></div>
                         </div>
                     </div>
-                    <div className="cpi-box-item2">
-                        <div><input value="Accept" type="submit" className="cpi-acceptBtn"></input></div>
-                    </div>
-                    <div className="cpi-box-item3">
-                        <div><input value="Reject" type="submit" className="cpi-rejectBtn"></input></div>
-                    </div>
+    
                 </div>
-
             </div>
-        </div>
-    );
-}
+        );
+    }
 
-export default CmpPending;
+}    
