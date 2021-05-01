d70238e0a4f52dcf799b5709540fe86de3525513
alanmatw <alanmatw@gmail.com>
2021-05-01 16:16:29 +0530

    feat(ProjectD): top nav - done!

--- zod/src/components/ProjectDashboard/ProjectD.js
+++ zod/src/components/ProjectDashboard/ProjectD.js
@@ -1,17 +1,48 @@
 import './ProjectD.css';
 import { Link } from "react-router-dom";
+import React from 'react';
  
 /* 
     ClassName Convention Used:-
         Eg: mp-top-nav -> MyProfile-top-nav ..
 */
 
-function ProjectD() {
-    return (
-        <div className="ProjectD">
-            <p>Welcome to PD...!</p>
-        </div>
-    );
-}
+export default class ProjectD extends React.Component {
 
-export default ProjectD;
+    constructor() {
+     
+        super();
+        this.state = {
+            data: ''
+        }
+    }
+
+    render() {
+    
+        return (
+            <div className="ProjectD">
+                
+                <div className="pd-top-nav">
+    
+                    <div className="pd-left-wrapper">
+                        <div className="pd-lt">
+                            <div className="pd-arrow"></div>
+                            <div><p className="pd-lt-txt">Back to Base Dashboard</p></div>
+                        </div>
+                        <div className="pd-lb"><p className="pd-title">zode</p></div>
+                    </div>
+    
+                    <div className="pd-mid-wrapper">
+                        <p>PROJECT&nbsp;&nbsp;DASHBOARD</p>
+                    </div>
+    
+                    <div className="pd-right-wrapper">
+                        <input type="submit" value="Logout" className="pd-logout-btn"></input>
+                    </div>
+    
+                </div>
+
+            </div>
+        );
+    }
+}
