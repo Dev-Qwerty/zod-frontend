411ddbfd29e85c6061998cda02e75e18f91b905d
jamesjose03 <jamesjose03@gmail.com>
2021-05-08 21:05:17 +0530

    chat default display restructured

--- zod/src/components/Chat/ChatHome.js
+++ zod/src/components/Chat/ChatHome.js
@@ -1,6 +1,7 @@
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import './ChatHome.css';
 import ChatSVG from '../../assets/Chat-Home.svg';
+import ChannelIcon from '../../assets/channel-icon.svg';
 
 function ChatHome() {
     return(
@@ -24,18 +25,40 @@ function ChatHome() {
                 </div>
             </div>
         </div>
-        <h1 className="ch-title">Conversations</h1>
-        <div className="chat-home-svg">
-            <img src={ChatSVG} alt="Chat SVG" className="ch-svg"></img>
-        </div>
-        <div className="ch-channel-wrapper">
-            <h3 className="ch-channel-title">Your Channels</h3>
-            <div className="ch-container">
-                <div className="ch-single-box-wrapper"></div>
-                <div className="ch-single-box-wrapper"></div>
+        <div className="ch-left-nav">
+                        
+            <div className="pd-left-nav-grid">
+                            
+                <div className="ch-lng1-wrapper">
+                        <div className="ch-lng1"></div>
+                </div>
+                <div className="pd-lng2"></div>
+                <Route render={({history})=> (<div className="ch-lng3" onClick={()=>{ history.push("/chat/createChannel")}}></div>)}></Route>
+                <div className="pd-lng4"></div>
+                <div className="pd-lng5"></div>
+                <div className="pd-lng6"></div>
+                <div className="pd-lng7"></div>
             </div>
         </div>
+        <div className="ch-leftnav-2">
+            <h2 className="ch-project-title">Project Name</h2>
+            <hr></hr>
+            <h3>Channels</h3>
+            <div className="ch-channels-list">
+                <a href="#">@everyone</a>
+                <a href="#">@frontend</a>
+            </div>
         </div>
+        <div className="ch-chat-display">
+            <div className="ch-default-display">
+                <img src={ChatSVG} alt="Chat SVG" className="ch-chat-svg"></img>
+                <div className="ch-chat-instrns">
+                    <span>Click on a channel or personal chat to begin a conversation.</span>
+                    <span>No channels created? Click <Link to="/chat/createChannel">here</Link> to create one.</span>
+                </div>
+            </div>
+        </div>
+    </div>
     )
 }
 
