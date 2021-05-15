9422262bda5d0648ed72bc16bab6328efec4331d
alanmatw <alanmatw@gmail.com>
2021-05-15 22:17:29 +0530

    fix(Board): left nav

--- zod/src/components/Board/Board.js
+++ zod/src/components/Board/Board.js
@@ -8,7 +8,7 @@ import ReactTooltip from "react-tooltip";
         Eg: mp-top-nav -> MyProfile-top-nav ..
 */
 
-export default class ProjectD extends React.Component {
+export default class Board extends React.Component {
 
     constructor() {
      
@@ -30,54 +30,54 @@ export default class ProjectD extends React.Component {
     render() {
     
         return (
-            <div className="ProjectD">
+            <div className="Board">
                 
-                <div className="pd-top-nav">
+                <div className="b-top-nav">
 
-                    <div className="pd-left-wrapper">
-                        <div className="pd-lt" onClick={ this.backToBaseFn }>
-                            <div className="pd-arrow"></div>
-                            <div><p className="pd-lt-txt">Back to Base Dashboard</p></div>
+                    <div className="b-left-wrapper">
+                        <div className="b-lt" onClick={ this.backToBaseFn }>
+                            <div className="b-arrow"></div>
+                            <div><p className="b-lt-txt">Back to Base Dashboard</p></div>
                         </div>
-                        <div className="pd-lb"><p className="pd-title">zode</p></div>
+                        <div className="b-lb"><p className="b-title">zode</p></div>
                     </div>
     
-                    <div className="pd-mid-wrapper">
+                    <div className="b-mid-wrapper">
                         <p>PROJECT&nbsp;&nbsp;DASHBOARD</p>
                     </div>
     
-                    <div className="pd-right-wrapper">
-                        <input type="submit" value="Logout" className="pd-logout-btn" onClick = { this.logout }></input>
+                    <div className="b-right-wrapper">
+                        <input type="submit" value="Logout" className="b-logout-btn" onClick = { this.logout }></input>
                     </div>
     
                 </div>
 
-                <div className="pd-body-wrapper">
+                <div className="b-body-wrapper">
 
-                    <div className="pd-left-nav">
+                    <div className="b-left-nav">
                         
-                        <div className="pd-left-nav-grid">
+                        <div className="b-left-nav-grid">
                             
                             <Link to="/projectdashboard/home" style={{ textDecoration: 'none' }}>
-                                <div className="pd-lng1-wrapper">
-                                    <div className="pd-lng1" data-tip data-for="homeTip"></div>
+                                <div className="b-lng1-wrapper">
+                                    <div className="b-lng1" data-tip data-for="homeTip"></div>
                                 </div>
                             </Link> 
 
                             <Link to="/projectdashboard/board" style={{ textDecoration: 'none' }}>
-                                <div className="pd-lng2" data-tip data-for="boardTip">
+                                <div className="b-lng2" data-tip data-for="boardTip">
                                 </div>
                             </Link>
 
                             <Link to="/chat/createChannel" style={{ textDecoration: 'none' }}>
-                                <div className="pd-chat" data-tip data-for="chatTip">
+                                <div className="b-lng3" data-tip data-for="chatTip">
                                 </div>
                             </Link>               
 
-                            <div className="pd-lng4" data-tip data-for="calTip"></div>
-                            <div className="pd-lng5" data-tip data-for="calTip"></div>
-                            <div className="pd-lng6" data-tip data-for="calTip"></div>
-                            <div className="pd-lng7" data-tip data-for="calTip"></div>
+                            <div className="b-lng4" data-tip data-for="calTip"></div>
+                            <div className="b-lng5" data-tip data-for="calTip"></div>
+                            <div className="b-lng6" data-tip data-for="calTip"></div>
+                            <div className="b-lng7" data-tip data-for="calTip"></div>
 
                             <ReactTooltip id="homeTip" place="right" effect="float" type="dark">Home</ReactTooltip> 
                             <ReactTooltip id="boardTip" place="right" effect="float" type="dark">Board</ReactTooltip>
@@ -87,10 +87,10 @@ export default class ProjectD extends React.Component {
                         </div>
                     </div>
                     
-                    <div className="pd-body">
+                    <div className="b-body">
                         
-                        <div className="pdb-hdn-wrapper">
-                            <p>BOARD!</p>
+                        <div className="bb-wrapper">
+                            
                         </div>
 
                     </div>
